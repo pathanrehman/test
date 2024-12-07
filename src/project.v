cac364f7a2cb695b53f7e479dacc468541f9951b
pathanrehman <52851945+pathanrehman@users.noreply.github.com>
2024-12-07 11:03:24 +0530

    Update counter.v

--- src/project.v
+++ src/project.v
@@ -1,27 +1,19 @@
-/*
- * Copyright (c) 2024 Your Name
- * SPDX-License-Identifier: Apache-2.0
- */
-
-`default_nettype none
-
-module tt_um_example (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
-);
-
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
-
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
-
+// Here we will learn to write a verilog HDL to design a 4 bit counter
+module counter(clk,reset,up_down,load,data,count);
+  //define input and output ports
+  input clk,reset,load,up_down;
+  input [3:0] data;
+  output reg [3:0] count;
+  //always block will be executed at each and every positive edge of the clock
+  always@(posedge clk) 
+  begin
+    if(reset)    //Set Counter to Zero
+      count <= 0;
+    else if(load)    //load the counter with data value
+      count <= data;
+    else if(up_down)        //count up
+      count <= count + 1;
+    else            //count down
+      count <= count - 1;
+  end
 endmodule
